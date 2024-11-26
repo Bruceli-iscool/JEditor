68025104d39d6bcb5e3844302ed3fe95193ff4c7
Bruce Li <isanoob1897@outlook.com>
2024-11-26 20:24:55 +0000

    added launcher controls

--- app/src/main/java/dev/desktop/JEditor/App.java
+++ app/src/main/java/dev/desktop/JEditor/App.java
@@ -1,11 +1,27 @@
 package dev.desktop.JEditor;
+import java.io.IOException;
+import java.awt.event.*;
 import javax.swing.*;
 public class App {
     public static void main(String args[]) {
         
     }
-    protected static void launcher() {
+    protected static void launcher() throws IOException {
         JFrame launcher = new JFrame("JEditor launcher");
         launcher.setSize(1200, 700);
+        JButton select = new JButton("Choose File");
+        select.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser n = new JFileChooser();
+                try {
+                    editor c = new editor(n.getSelectedFile().getAbsolutePath());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        launcher.add(select);
+        // add default close operation
     }
 }
--- app/src/main/java/dev/desktop/JEditor/editor.java
+++ app/src/main/java/dev/desktop/JEditor/editor.java
@@ -1,16 +1,19 @@
 package dev.desktop.JEditor;
-import java.io.File;
 import java.io.IOException;
 
 import javax.swing.*;
 import java.nio.file.*;
 public class editor {
-    File f;
+    String f;
     JFrame n;
     String content;
     public editor(String filename) throws IOException {
         n = new JFrame("JEditor " + filename);
+        f = filename;
         n.setSize(1600, 900);
         content = Files.readString(Paths.get(filename));
+    }
+    public void open() {
+        n = new JFrame("JEditor " + f);
     }
 }
